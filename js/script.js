40d1b3ecea45a23123b0341e5f204099f5a50286
Greg Mohr <gregmohr2000@yahoo.com>
2022-01-28 15:58:45 -0800

    added tooltips to Home contact icons

--- js/script.js
+++ js/script.js
@@ -3,3 +3,8 @@ window.addEventListener('scroll', stickyNav);
 const stickyNav = event => {
   console.log(event);
 }
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
